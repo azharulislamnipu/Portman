9de185338349a8ca53a73b1e48e87c7bbcc3f897
azhar <azharulIslmanipu@gmail.com>
2019-05-12 13:50:39 +0600

    blog Completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,35 @@
     "use strict";
     
     /*--
-    Menu Stick
+    blog slider
     -----------------------------------*/
+    $(".blog-slider").slick({
+        dots: false,
+        arrows:false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fas fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fas fa-angle-right' aria-hidden='true'></i></button>",
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: true,
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: true,
+
+                    slidesToShow: 1
+                }
+            }
+        ]
+
+    });
    
     
     
